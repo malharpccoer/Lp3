a7cd8b3fd987c6622e125d284159319714d50a8c
pccoer25 <malhargujar588@gmail.com>
2024-11-12 20:53:39 +0530

    Update ML 3.py

--- ML/ML 3.py
+++ ML/ML 3.py
@@ -1,3 +1,15 @@
+"Given a bank customer, build a neural network-based classifier that can determine whether they will
+leave or not in the next 6 months.
+Dataset Description: The case study is from an open-source dataset from Kaggle. The dataset contains
+10,000 sample points with 14 distinct features such as CustomerId, CreditScore, Geography, Gender,
+Age, Tenure, Balance, etc. Perform following steps:
+1. Read the dataset.
+2. Distinguish the feature and target set and divide the data set into training and test sets.
+3. Normalize the train and test data.
+4. Initialize and build the model. Identify the points of improvement and implement the same.
+5. Print the accuracy score and confusion matrix (5 points)."
+
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
